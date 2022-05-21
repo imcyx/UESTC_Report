5f6159856c67678f9dd8b4917955bc8151b3687d
ChenYuxi <im.cyx@foxmail.com>
2022-05-22 01:32:12 +0800

    install scripts

---
+++ shell/firefox_install.sh
@@ -0,0 +1,5 @@
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/89.0/linux-x86_64/zh-CN/firefox-89.0.tar.bz2;
+tar -jxf firefox-*.tar.bz2;
+mv firefox /opt;
+ln -s /opt/firefox/firefox /usr/local/bin/firefox;
+firefox --version;
---
+++ shell/geckodriver_install.sh
@@ -0,0 +1,4 @@
+wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz;
+tar -xzf geckodriver-*.tar.gz;
+chmod +x geckodriver;
+cp geckodriver /usr/local/bin/;
